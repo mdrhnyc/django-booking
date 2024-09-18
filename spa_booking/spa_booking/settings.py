63c8b052f27faa854f3617481a39267424d287e1
Md Raqueebul Hussain <raqueeb@raqueebs-mbp.mynetworksettings.com>
2024-09-17 20:55:31 -0400

    added cors settings

--- spa_booking/spa_booking/settings.py
+++ spa_booking/spa_booking/settings.py
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'appointments',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -130,3 +132,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000'
+]
